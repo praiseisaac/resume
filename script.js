ffa60d982707c49803757779500700f70f468449
praise-hvl <praise@harmonyventurelabs.com>
2021-10-13 04:27:27 -0500

    added pdf link

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 import { jsPDF } from "jspdf";
 
-const print = () => {
+const printPdf = () => {
   var doc = new jsPDF();
   var elementHandler = {
     "#ignorePDF": function (element, renderer) {
       return true;
     },
   };
-  var source = window.document.getElementsByTagName("body")[0];
+  var source = document.getElementsById("main-container");
   doc.fromHTML(source, 15, 15, {
     width: 180,
     elementHandlers: elementHandler,
